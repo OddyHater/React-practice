329de87680c30f5d1beb70acb80bd4eef9c87a77
Roman Grishin <roman.grishin.me@gmail.com>
2023-04-09 17:33:53 +0600

    component created

--- script.jsx
+++ script.jsx
@@ -1,15 +1,19 @@
-// Данные для списка
+// Компонент User
+function User(props) {
+  return (
+    <p>
+      <img src={`https://code.s3.yandex.net/web-code/react/${props.id}.png`} width="75" />
+      <br /><b>{props.name}</b>
+    </p>
+  );
+}
 
-const list = [{
-  name: 'Хлеб',
-  quantity: '1 батон',
-}, {
-  name: 'Ром',
-  quantity: '3 бутылки',
-}, {
-  name: 'Кока-кола',
-  quantity: '3 бутылки',
-}, {
-  name: 'Туалетная бумага',
-  quantity: 'Вся, что есть',
-}];
+// Основной код приложения
+ReactDOM.render((
+  <>
+    <h2>Мои воображаемые друзья:</h2>
+    <User id="1" name="Gregory" />
+    <User id="2" name="James" />
+    <User id="3" name="Allison" />
+  </>
+), document.querySelector('#root'));
