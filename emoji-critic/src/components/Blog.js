62b27d6338b00e7de8667e0356108c8b377f836e
Roman Grishin <roman.grishin.me@gmail.com>
2023-04-30 01:16:30 +0600

    json practice

--- emoji-critic/src/components/Blog.js
+++ emoji-critic/src/components/Blog.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Post from "./Post";
 
 function Blog() {
   const [posts, setPosts] = useState([]);
